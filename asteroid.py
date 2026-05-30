2938e423efcae6489ea61514347ba53cde3a92d0
tomwolvo <tom.wolverson@gmail.com>
2026-05-30 11:54:00 +1000

    Added asteroids

---
+++ asteroid.py
@@ -0,0 +1,12 @@
+from circleshape import CircleShape
+import pygame
+from constants import LINE_WIDTH
+
+class Asteroid(CircleShape):
+    def __init__(self, x: float, y: float, radius: float) -> None:
+        super().__init__(x, y, radius)
+    def draw(self, screen) -> None:
+        pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+    def update(self, dt: float) -> None:
+        self.position += self.velocity * dt
+    
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -11,15 +13,26 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    pygame.display.set_caption("Asteroids")
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("Black")
-        player.draw(screen)
-        player.update(dt)
+        screen.fill("black")
+        updatable.update(dt)
+        for obj in drawable:
+                obj.draw(screen)
         pygame.display.flip()            
         dt = clock.tick(60) / 1000.0
 
